f9d82cb3f6e8bd65413ba15c910e493491bbeef4
Josh XT <josh@devxt.com>
2023-07-05 12:24:24 -0400

    Add AGIXT_API_KEY

--- extensions/agixt_actions.py
+++ extensions/agixt_actions.py
@@ -26,7 +26,13 @@ from typing import List
 from Extensions import Extensions
 from agixtsdk import AGiXTSDK
 
-ApiClient = AGiXTSDK(base_uri="http://localhost:7437")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+agixt_api_key = os.getenv("AGIXT_API_KEY")
+base_uri = "http://localhost:7437"
+ApiClient = AGiXTSDK(base_uri=base_uri, api_key=agixt_api_key)
 
 
 class agixt_actions(Extensions):
--- provider/agixt.py
+++ provider/agixt.py
@@ -1,6 +1,11 @@
 from agixtsdk import AGiXTSDK
+import os
+from dotenv import load_dotenv
 
-ApiClient = AGiXTSDK(base_uri="http://localhost:7437")
+load_dotenv()
+agixt_api_key = os.getenv("AGIXT_API_KEY")
+base_uri = "http://localhost:7437"
+ApiClient = AGiXTSDK(base_uri=base_uri, api_key=agixt_api_key)
 
 
 # This will create a hive agent that will create memories for itself and each agent in the rotation.
