e6177670d096952571aec1bf048fb08a4e28f4bb
Josh XT <josh@devxt.com>
2023-08-21 17:17:01 -0400

    Update negative prompt

--- extensions/stable_diffusion.py
+++ extensions/stable_diffusion.py
@@ -40,7 +40,7 @@ class stable_diffusion:
         self,
         prompt: str,
         filename: str = "",
-        negative_prompt: str = "out of frame,lowres,text,error,cropped,worst quality,low quality,jpeg artifacts,ugly,duplicate,morbid,mutilated,out of frame,extra fingers,mutated hands,poorly drawn hands,poorly drawn face,mutation,deformed,blurry,dehydrated,bad anatomy,bad proportions,extra limbs,cloned face,disfigured,gross proportions,malformed limbs,missing arms,missing legs,extra arms,extra legs,fused fingers,too many fingers,long neck,username,watermark,signature,",
+        negative_prompt: str = "out of frame,lowres,text,error,cropped,worst quality,low quality,jpeg artifacts,ugly,duplicate,morbid,mutilated,out of frame,extra fingers,mutated hands,poorly drawn hands,poorly drawn face,mutation,deformed,blurry,dehydrated,bad anatomy,bad proportions,extra limbs,cloned face,disfigured,gross proportions,malformed limbs,missing arms,missing legs,extra arms,extra legs,fused fingers,too many fingers,long neck,username,watermark,signature",
         batch_size: int = 1,
         cfg_scale: int = 7,
         denoising_strength: int = 0,
@@ -91,7 +91,7 @@ class stable_diffusion:
                 "prompt": prompt,
                 "negative_prompt": negative_prompt
                 if negative_prompt
-                else "out of frame,lowres,text,error,cropped,worst quality,low quality,jpeg artifacts,ugly,duplicate,morbid,mutilated,out of frame,extra fingers,mutated hands,poorly drawn hands,poorly drawn face,mutation,deformed,blurry,dehydrated,bad anatomy,bad proportions,extra limbs,cloned face,disfigured,gross proportions,malformed limbs,missing arms,missing legs,extra arms,extra legs,fused fingers,too many fingers,long neck,username,watermark,signature,",
+                else "out of frame,lowres,text,error,cropped,worst quality,low quality,jpeg artifacts,ugly,duplicate,morbid,mutilated,out of frame,extra fingers,mutated hands,poorly drawn hands,poorly drawn face,mutation,deformed,blurry,dehydrated,bad anatomy,bad proportions,extra limbs,cloned face,disfigured,gross proportions,malformed limbs,missing arms,missing legs,extra arms,extra legs,fused fingers,too many fingers,long neck,username,watermark,signature",
                 "batch_size": batch_size if batch_size else 1,
                 "cfg_scale": cfg_scale if cfg_scale else 7,
                 "denoising_strength": denoising_strength if denoising_strength else 0,
