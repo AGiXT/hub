154db7cfab1bb000355abf388b3229149e993524
Josh XT <josh@devxt.com>
2023-08-22 14:56:41 -0400

    Change default filename to uuid if empty

--- extensions/stable_diffusion.py
+++ extensions/stable_diffusion.py
@@ -1,8 +1,7 @@
-import os
+import uuid
 from io import BytesIO
 from Extensions import Extensions
 import requests
-import numpy as np
 import base64
 import io
 
@@ -38,7 +37,7 @@ class stable_diffusion(Extensions):
     async def generate_image(
         self,
         prompt: str,
-        filename: str = "image.png",
+        filename: str = "",
         negative_prompt: str = "out of frame,lowres,text,error,cropped,worst quality,low quality,jpeg artifacts,ugly,duplicate,morbid,mutilated,out of frame,extra fingers,mutated hands,poorly drawn hands,poorly drawn face,mutation,deformed,blurry,dehydrated,bad anatomy,bad proportions,extra limbs,cloned face,disfigured,gross proportions,malformed limbs,missing arms,missing legs,extra arms,extra legs,fused fingers,too many fingers,long neck,username,watermark,signature",
         batch_size: int = 1,
         cfg_scale: int = 7,
@@ -65,6 +64,8 @@ class stable_diffusion(Extensions):
         tiling: bool = False,
         width: int = 768,
     ) -> str:
+        if filename == "":
+            filename = f"{uuid.uuid4()}.png"
         image_path = f"./WORKSPACE/{filename}"
         headers = {}
         if (
