4d9e59ccbeafafe6b3d04ecaf894c878b0837789
Josh XT <josh@devxt.com>
2023-08-21 20:05:12 -0400

    Remove logging

--- extensions/stable_diffusion.py
+++ extensions/stable_diffusion.py
@@ -111,9 +111,6 @@ class stable_diffusion(Extensions):
                 "tiling": tiling if tiling else False,
                 "width": width if width else 1920,
             }
-        logging.info(f"Generating image for prompt: {prompt}")
-        logging.info(f"Hugging Face API Key: {self.HUGGINGFACE_API_KEY}")
-        logging.info(f"Stable Diffusion API URL: {self.STABLE_DIFFUSION_API_URL}")
         try:
             response = requests.post(
                 self.STABLE_DIFFUSION_API_URL,
