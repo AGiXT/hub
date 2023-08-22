2d5cee102451b1aef37ccbd3391ad1e61710a5db
Josh XT <josh@devxt.com>
2023-08-22 05:18:05 -0400

    Clean up dalle references

--- extensions/dalle.py
+++ extensions/dalle.py
@@ -16,31 +16,27 @@ class dalle(Extensions):
     def __init__(
         self,
         OPENAI_API_KEY: str = "",
-        WORKING_DIRECTORY: str = "./WORKSPACE",
         **kwargs,
     ):
         self.OPENAI_API_KEY = OPENAI_API_KEY
-        self.WORKING_DIRECTORY = WORKING_DIRECTORY
         if self.OPENAI_API_KEY:
             self.commands = {
                 "Generate Image with DALLE": self.generate_image_with_dalle
             }
 
-    async def generate_image_with_dalle(self, prompt: str, filename: str) -> str:
+    async def generate_image_with_dalle(
+        self, prompt: str, filename: str = "image.png"
+    ) -> str:
+        image_path = f"./WORKSPACE/{filename}"
         openai.api_key = self.OPENAI_API_KEY
-
         response = openai.Image.create(
             prompt=prompt,
             n=1,
             size="256x256",
             response_format="b64_json",
         )
-
         logging.info(f"Image Generated for prompt:{prompt}")
-
         image_data = b64decode(response["data"][0]["b64_json"])
-
-        with open(f"{self.WORKING_DIRECTORY}/{filename}", mode="wb") as png:
+        with open(image_path, mode="wb") as png:
             png.write(image_data)
-
         return f"Saved to disk:{filename}"
